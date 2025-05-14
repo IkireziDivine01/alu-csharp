4f071f74e2d364f56aa85c11335e89d819f7eb7a
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-14 10:49:53 +0200

    ifelse loops

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder output = new StringBuilder();
+        bool isFirst = true;
+        
+        for (int first = 0; first <= 8; first++)
+        {
+            for (int second = first + 1; second <= 9; second++)
+            {
+                if (!isFirst)
+                {
+                    output.Append(", ");
+                }
+                
+                output.Append($"{first}{second}");
+                isFirst = false;
+            }
+        }
+        
+        Console.WriteLine(output.ToString());
+    }
+}
