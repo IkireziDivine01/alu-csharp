7136e8cc996d780a3f0797ff7adf221a6bf03a1f
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-15 17:00:24 +0200

    5 fix

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -5,11 +5,30 @@ class Program
     static void Main(string[] args)
     {
         string output = "";
+        char lastChar = '\0';
         
+        // Find the last valid character (not 'q' or 'e') in the range
+        for (char c = 'z'; c >= 'a'; c--)
+        {
+            if (c != 'q' && c != 'e')
+            {
+                lastChar = c;
+                break;
+            }
+        }
+        
+        // Build the output string with commas between letters
         for (char c = 'a'; c <= 'z'; c++)
         {
             if (c != 'q' && c != 'e')
+            {
                 output += c;
+                // Add comma only if this is not the last character
+                if (c != lastChar)
+                {
+                    output += ",";
+                }
+            }
         }
         
         Console.Write(output);
