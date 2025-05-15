ef35e2a99715cc91197582807b7def86d34d9f7a
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-15 17:06:33 +0200

    5 fix

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -8,15 +8,10 @@ class Program
         
         for (int i = 0; i < 100; i++)
         {
-            // Format each number with two digits
             output += i.ToString("D2");
-            
-            // Add comma and space after each number, except the last one
-            if (i < 99)
-                output += ", ";
+            output += (i == 99) ? "" : ", ";
         }
         
-        // Print all formatted numbers at once and add a new line
         Console.WriteLine(output);
     }
 }
