15979405eee58c479ad08257214756113222fcba
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-15 12:48:10 +0200

    --amend

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -5,4 +5,4 @@ mkdir -p 2-new_project
 cd 2-new_project
 dotnet new console
 dotnet restore
-dotnet build
+dotnet build --nologo | grep -E "Build succeeded.|[0-9]+ Warning\(s\)|[0-9]+ Error\(s\)" | tr -d '\n'
