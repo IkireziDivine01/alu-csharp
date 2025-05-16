d913288ce4535ea8cd0ed140a4ef5316ce7b7d17
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-16 10:50:21 +0200

    1

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-dotnet new console -o 1-new_project
+dotnet new console -o 1-new_project --framework netcoreapp2.1
 
 dotnet build 1-new_project/1-new_project.csproj -v detailed
