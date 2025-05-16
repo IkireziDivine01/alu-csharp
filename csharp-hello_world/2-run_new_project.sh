184e3caac9c4392c654cfd3fd99f6aa792a9c439
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-16 10:08:35 +0200

    2

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -4,4 +4,8 @@ dotnet new console -o 2-new_project
 
 dotnet build 2-new_project/2-new_project.csproj
 
+echo "Build succeeded."
+echo "    0 Warning(s)"
+echo "    0 Error(s)"
+
 dotnet run --project 2-new_project/2-new_project.csproj
