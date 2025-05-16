e39b3174b2edc28be0041c8b969d5327bd80cac5
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-16 08:04:34 +0200

    Feat: running

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p 1-new_project
+dotnet new console -o 1-new_project
 
-cd 1-new_project
-
-dotnet new console
-
-dotnet build
-
-cd ..
+dotnet build 1-new_project/1-new_project.csproj
