4e8294acc23c375803f10d1028a9516b24643996
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-21 00:18:58 +0200

    3 fix

---
+++ csharp-arrays_lists_dictionaries/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+// This file is used by Code Analysis to maintain SuppressMessage attributes
+// specific to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CS8625:Cannot convert null literal to non-nullable reference type", Justification = "Deliberate test case")]
+#nullable enable
