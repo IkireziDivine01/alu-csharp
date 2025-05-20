45b01a53093a28ee12a917f0e8477e2446a3dd64
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-20 23:43:39 +0200

    3 fix

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 
-public class Array
+#nullable enable // Enable nullable reference types
+
+class Array
 {
     public static void Reverse(int[] array)
     {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
         for (int i = array.Length - 1; i >= 0; i--)
         {
-            Console.Write(array[i] + " ");
+            Console.Write(array[i]);
+            if (i > 0)
+                Console.Write(" ");
         }
         Console.WriteLine();
+    }
+    
+    // This method will call Reverse with a null parameter, triggering the warning
+    public static void Test()
+    {
+        int[]? nullArray = null;
+        Reverse(nullArray); // This will trigger the warning CS8604
     }
 }
