ebc0210fd1b3796cb31cb030dec26e96e2910a4a
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-20 21:52:19 +0200

    0

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,7 +4,7 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        
+
         if (index < 0 || index >= array.Length)
         {
             Console.WriteLine("Index out of range");
@@ -14,31 +14,5 @@ class Array
         array[index] = n;
         
         return array;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] array = {0, 1, 2, 3, 4, 5, 6};
-
-        PrintArray(array);
-        Array.ReplaceElement(array, 1, 98);
-        PrintArray(array);
-    }
-
-    static void PrintArray(int[] array)
-    {
-        int i;
-
-        for (i = 0; i < array.Length; i++)
-        {
-            Console.Write(array[i]);
-            if (i < array.Length - 1)
-                Console.Write(" ");
-        }
-
-        Console.WriteLine();
     }
 }
