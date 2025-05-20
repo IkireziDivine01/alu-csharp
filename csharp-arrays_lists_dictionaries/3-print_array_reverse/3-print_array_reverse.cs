290608deb1a1156582cd75be4ab3b8ca902bd338
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-21 00:17:55 +0200

    3 fix

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 
-// Ensure nullable reference types are enabled
 #nullable enable
 
 class Array
 {
-    // Explicitly mark parameter as non-nullable to trigger warning
-    public static void Reverse(
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-        [System.Diagnostics.CodeAnalysis.DisallowNull] int[] array = null
-#pragma warning restore CS8625
-    )
+    public static void Reverse(int[] array)
     {
         if (array == null || array.Length == 0)
         {
