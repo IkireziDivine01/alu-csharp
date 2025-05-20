cdf92b59547745649268e7fe557bf3401d2e7b25
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-20 23:25:39 +0200

    3 fix

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,23 +4,18 @@ class Array
 {
     public static void Reverse(int[] array)
     {
-        // Explicitly using System.Array.Reverse which will generate warnings for null values
-        System.Array.Reverse(array);
-        
-        if (array.Length == 0)
+        if (array == null || array.Length == 0)
         {
             Console.WriteLine();
             return;
         }
-        
+
         for (int i = array.Length - 1; i >= 0; i--)
         {
             Console.Write(array[i]);
-            
             if (i > 0)
                 Console.Write(" ");
         }
-        
         Console.WriteLine();
     }
 }
