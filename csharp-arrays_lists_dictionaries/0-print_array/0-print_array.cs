eda00097fa6cb5169dd6e8ef63d1540086abef06
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-20 21:44:08 +0200

    arrays manupulation

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] newArray;
+
+        newArray = Array.CreatePrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(-10);
+    }
+}
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        
+        int[] newArray = new int[size];
+        
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+        }
+        
+        if (size > 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(newArray[i]);
+                if (i < size - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+        
+        return newArray;
+    }
+}
