7168f550e3e7102d7c52ca420f0614d87efbe91e
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-28 10:31:35 +0200

    01-common-element

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,23 @@
+﻿public static class List {
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        if (list1 == null || list2 == null)
+            return new List<int>();
+
+        List<int> commonElements -new List<int>();
+        HashSet<int> set2 = new HashSet<int>(list2);
+        HashSet<int> addedElements = new HashSet<int>();
+
+        foreach (int element in list1) {
+            if (set2.Contains(element) && !addedElements.Contains(element))
+            {
+                commonElements.Add(element);
+                addedElements.Add(element);
+            }
+        }
+
+        commonElements.Sort();
+
+        return commonElements;
+    }
+}
