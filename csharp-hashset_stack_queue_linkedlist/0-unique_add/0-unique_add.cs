6ea8449d37ae38b079e7d0ed00302c0c3a3dbedb
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-05-28 10:15:20 +0200

    add list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class List
+{
+    public static int Sum(List<int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return 0;
+        
+        return myList.Distinct().Sum();
+    }
+}
