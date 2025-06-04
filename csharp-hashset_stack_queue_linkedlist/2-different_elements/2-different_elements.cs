7da54c166cbbd0e714af48bfe7244e08a41d65a5
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-04 09:36:51 +0200

    2 and 3

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        if (list1 == null && list2 == null)
+            return new List<int>();
+        
+        if (list1 == null)
+        {
+            List<int> result = new List<int>(new HashSet<int>(list2));
+            result.Sort();
+            return result;
+        }
+        
+        if (list2 == null)
+        {
+            List<int> result = new List<int>(new HashSet<int>(list1));
+            result.Sort();
+            return result;
+        }
+        
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+        List<int> differentElements = new List<int>();
+        
+        foreach (int element in set1)
+        {
+            if (!set2.Contains(element))
+            {
+                differentElements.Add(element);
+            }
+        }
+        
+        foreach (int element in set2)
+        {
+            if (!set1.Contains(element))
+            {
+                differentElements.Add(element);
+            }
+        }
+        
+        differentElements.Sort();
+        
+        return differentElements;
+    }
+}
---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        
+        bool containsSearch = false;
+        foreach (string item in aStack)
+        {
+            if (item == search)
+            {
+                containsSearch = true;
+                break;
+            }
+        }
+        
+        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
+        
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            
+            while (aStack.Count > 0)
+            {
+                string currentItem = aStack.Pop(); 
+                if (currentItem == search)
+                {
+                    break;
+                }
+            }
+            
+            aStack.Clear();
+            
+            Stack<string> reverseTemp = new Stack<string>();
+            while (tempStack.Count > 0)
+            {
+                reverseTemp.Push(tempStack.Pop());
+            }
+            
+            while (reverseTemp.Count > 0)
+            {
+                aStack.Push(reverseTemp.Pop());
+            }
+        }
+        
+        aStack.Push(newItem);
+        
+        return aStack;
+    }
+}
