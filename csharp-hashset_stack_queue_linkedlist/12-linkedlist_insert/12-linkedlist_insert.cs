0b50cfad06c5245d74991878654bcc024875463a
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-05 12:49:37 +0200

    remaining

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0 || n <= myLList.First.Value)
+        {
+            return myLList.AddFirst(n);
+        }
+        
+        if (n >= myLList.Last.Value)
+        {
+            return myLList.AddLast(n);
+        }
+        
+        LinkedListNode<int> current = myLList.First;
+        while (current.Next != null)
+        {
+            if (n <= current.Next.Value)
+            {
+                return myLList.AddBefore(current.Next, n);
+            }
+            current = current.Next;
+        }
+        
+        return myLList.AddLast(n);
+    }
+}
