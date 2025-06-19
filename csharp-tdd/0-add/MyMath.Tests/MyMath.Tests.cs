4f2a76159f2a1f4f94ca844567736ebd5295a68a
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-19 12:43:46 +0200

    tdd

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    public class OperationsTests
+    {
+        [Fact]
+        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+        {
+            int result = Operations.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Add_NegativeAndPositiveNumber_ReturnsCorrectSum()
+        {
+            int result = Operations.Add(-4, 6);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Add_TwoNegativeNumbers_ReturnsCorrectSum()
+        {
+            int result = Operations.Add(-2, -3);
+            Assert.Equal(-5, result);
+        }
+
+        [Fact]
+        public void Add_ZeroAndNumber_ReturnsSameNumber()
+        {
+            int result = Operations.Add(0, 7);
+            Assert.Equal(7, result);
+        }
+    }
+}
