6e964b8fab2f06d238c0e2dc1dc091382530d630
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-22 01:32:30 +0200

    0

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -3,13 +3,34 @@ using MyMath;
 
 namespace MyMath.Tests
 {
-    [TestFixture]
-    public class MyMathTests
+    public class Tests
     {
         [Test]
-        public void Add_BasicTest()
+        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
         {
-            Assert.AreEqual(5, Operations.Add(2, 3));
+            int result = Operations.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Add_NegativeAndPositive_ReturnsCorrectSum()
+        {
+            int result = Operations.Add(-2, 3);
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Add_TwoNegativeNumbers_ReturnsCorrectSum()
+        {
+            int result = Operations.Add(-2, -3);
+            Assert.AreEqual(-5, result);
+        }
+
+        [Test]
+        public void Add_ZeroAndNumber_ReturnsSameNumber()
+        {
+            int result = Operations.Add(0, 5);
+            Assert.AreEqual(5, result);
         }
     }
 }
--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,16 +1,12 @@
+﻿using System;
+
 namespace MyMath
 {
-    /// <summary>
-    /// Class containing mathematical operations
-    /// </summary>
     public class Operations
     {
         /// <summary>
-        /// Adds two integers together
+        /// Adds two integers and returns the result.
         /// </summary>
-        /// <param name="a">First integer to add</param>
-        /// <param name="b">Second integer to add</param>
-        /// <returns>The sum of a and b</returns>
         public static int Add(int a, int b)
         {
             return a + b;
