d6e7c78c08ada3c425f0db4f72b19fe3d8a09518
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-22 01:35:06 +0200

    0

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -9,28 +9,28 @@ namespace MyMath.Tests
         public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
         {
             int result = Operations.Add(2, 3);
-            Assert.AreEqual(5, result);
+            Assert.That(result, Is.EqualTo(5));
         }
 
         [Test]
         public void Add_NegativeAndPositive_ReturnsCorrectSum()
         {
             int result = Operations.Add(-2, 3);
-            Assert.AreEqual(1, result);
+            Assert.That(result, Is.EqualTo(1));
         }
 
         [Test]
         public void Add_TwoNegativeNumbers_ReturnsCorrectSum()
         {
             int result = Operations.Add(-2, -3);
-            Assert.AreEqual(-5, result);
+            Assert.That(result, Is.EqualTo(-5));
         }
 
         [Test]
         public void Add_ZeroAndNumber_ReturnsSameNumber()
         {
             int result = Operations.Add(0, 5);
-            Assert.AreEqual(5, result);
+            Assert.That(result, Is.EqualTo(5));
         }
     }
 }
