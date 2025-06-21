0ad5752bb04c45edd43b100d09bed1df065232d0
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-22 00:52:02 +0200

    xml 0-fix

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,7 +1,16 @@
 namespace MyMath
 {
+    /// <summary>
+    /// Class containing mathematical operations
+    /// </summary>
     public class Operations
     {
+        /// <summary>
+        /// Adds two integers together
+        /// </summary>
+        /// <param name="a">First integer to add</param>
+        /// <param name="b">Second integer to add</param>
+        /// <returns>The sum of a and b</returns>
         public static int Add(int a, int b)
         {
             return a + b;
