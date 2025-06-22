3812f41748743fe92c796774c21e06bbcb71a43b
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-06-22 02:02:47 +0200

    0

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,6 +2,12 @@
 {
     public class Operations
     {
+        /// <summary>
+        /// Adds two integers and returns their sum.
+        /// </summary>
+        /// <param name="a">First integer to add.</param>
+        /// <param name="b">Second integer to add.</param>
+        /// <returns>The sum of <paramref name="a"/> and <paramref name="b"/>.</returns>
         public static int Add(int a, int b)
         {
             return a + b;
