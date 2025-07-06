4edb6c8d22d3abc8bda359b08bdbe9c549a74704
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-07-06 13:02:49 +0200

    3

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -15,72 +15,15 @@ public class Obj
         Type type = myObj.GetType();
 
         Console.WriteLine($"{type.Name} Properties:");
-        PropertyInfo[] properties = type.GetProperties();
-        foreach (PropertyInfo prop in properties)
+        foreach (PropertyInfo prop in type.GetProperties())
         {
             Console.WriteLine(prop.Name);
         }
 
         Console.WriteLine($"{type.Name} Methods:");
-        MethodInfo[] methods = type.GetMethods();
-        foreach (MethodInfo method in methods)
+        foreach (MethodInfo method in type.GetMethods())
         {
             Console.WriteLine(method.Name);
         }
-    }
-}
-
-/// <summary>
-/// Represents a dog with basic properties and behaviors.
-/// </summary>
-public class Dog
-{
-    // Initialize the field with a default value to avoid nullable warning
-    private string name = "";
-    private int age;
-    private int happiness;
-
-    /// <summary>
-    /// Gets or sets the name of the dog.
-    /// </summary>
-    public string Name 
-    { 
-        get { return name; } 
-        set { name = value; } 
-    }
-
-    /// <summary>
-    /// Gets or sets the age of the dog.
-    /// </summary>
-    public int Age 
-    { 
-        get { return age; } 
-        set { age = value; } 
-    }
-
-    /// <summary>
-    /// Gets or sets the happiness level of the dog.
-    /// </summary>
-    public int Happiness 
-    { 
-        get { return happiness; } 
-        set { happiness = value; } 
-    }
-
-    /// <summary>
-    /// Makes the dog bark.
-    /// </summary>
-    public void Bark()
-    {
-        Console.WriteLine("Woof!");
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var a = new Dog();
-        Obj.Print(a);
     }
 }
