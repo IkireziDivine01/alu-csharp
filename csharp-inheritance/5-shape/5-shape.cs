14ee145d24e1ffa9e81fb8dcc30a60e99bb1a750
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-07-06 12:36:05 +0200

    6 fix

--- csharp-inheritance/5-shape/5-shape.cs
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -1,10 +1,15 @@
-﻿namespace ShapesLib
+﻿using System;
+
+namespace ShapesLib
 {
     public class Shape
     {
         public virtual int Area()
         {
             throw new NotImplementedException("Area() is not implemented");
+        }
+         static void Main(string[] args)
+        {
         }
     }
 }
