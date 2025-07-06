4da762250be2e133bd77579471f7310582f72315
IkireziDivine01 <ikirezidivine21@gmail.com>
2025-07-06 10:55:45 +0200

    inheritance 0-5

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,34 @@
+﻿﻿using System.Reflection;
+
+/// <summary>
+/// Provides utility methods for inspecting object properties and methods.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Prints the properties and methods of the specified object.
+    /// </summary>
+    /// <param name="myObj">The object to inspect.</param>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        Console.WriteLine($"{t.Name} Properties:");
+
+        foreach (PropertyInfo n in pList)
+        {
+            Console.WriteLine(n.Name);
+        }
+
+        IEnumerable<MethodInfo> nMethod = t.GetMethods();
+        Console.WriteLine($"{t.Name} Methods:");
+
+        foreach (MethodInfo m in nMethod)
+        {
+            if (m.DeclaringType == myObj.GetType())
+            {
+                Console.WriteLine(m.Name);
+            }
+        }
+    }
+}
